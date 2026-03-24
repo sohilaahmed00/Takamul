2e46cd8bc426fe0ee6a110a01a96eedddedac191
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-03-24 06:00:59 +0200

    space

--- src/pages/CreateSalesInvoice.tsx
+++ src/pages/CreateSalesInvoice.tsx
@@ -389,6 +389,7 @@ const CreateSalesInvoice: React.FC = () => {
                         if (discType === "fixed") itemTotal -= discValue;
                         if (discType === "percentage") itemTotal -= itemTotal * (discValue / 100);
 
+                        
                         return (
                           <div key={item.id} className="grid grid-cols-1 md:grid-cols-[2.5fr_1fr_1.5fr_1.5fr_1.5fr_1fr_1fr_40px] gap-4 p-4 md:p-2 bg-zinc-50 md:bg-transparent rounded-xl md:rounded-none border md:border-none border-zinc-100 items-center group transition-colors hover:bg-zinc-50/80">
                             <Controller
