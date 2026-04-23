4b6f08e0e603ef7f499dbb052d9e65834e23601e
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-04-23 20:31:39 +0200

    done

--- src/features/users/hooks/useGetAllUsers.ts
+++ src/features/users/hooks/useGetAllUsers.ts
@@ -2,9 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { GetAllUsersResponse } from "../types/users.types";
 import { usersKeys } from "../keys/users.keys";
 import { getAllUsers } from "../services/users";
+import { handleEmptyResponse } from "@/lib/handleEmptyResponse";
 
 export const useGetAllUsers = ({ page, limit, searchTerm }: { page: number; limit: number; searchTerm?: string }) =>
   useQuery<GetAllUsersResponse>({
     queryKey: usersKeys.list({ page, limit, searchTerm }),
-    queryFn: () => getAllUsers({ page, limit, searchTerm }),
+    queryFn: async () => {
+      try {
+        return await getAllUsers({ page, limit, searchTerm });
+      } catch (error) {
+        return handleEmptyResponse(error, { limit, page });
+      }
+    },
   });
