2989d81df15a668916e7a87728a9e0029d68b395
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-04-26 00:27:38 +0300

    done

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { createAuthRefresh } from "axios-auth-refresh";
 import { useAuthStore } from "@/store/authStore";
-import { refreshToken } from "@/features/auth/services/auth";
 import { jwtDecode } from "jwt-decode";
 import { AppJwtPayload } from "@/types";
+import { LoginResponse } from "@/features/auth/types/auth.types";
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -18,12 +17,69 @@ apiClient.interceptors.request.use((config) => {
   return config;
 });
 
-const refreshAuthLogic = async (failedRequest: any) => {
-  const data = await refreshToken();
-  const decoded = jwtDecode<AppJwtPayload>(data.accessToken);
-  useAuthStore.getState().setAuth(data.accessToken, new Date(data.accessTokenExpiration).getTime(), decoded.Permission, decoded?.UserId, decoded?.email, decoded?.username);
-  failedRequest.response.config.headers.Authorization = `Bearer ${data.accessToken}`;
-  return Promise.resolve();
+// Source - https://stackoverflow.com/a/58282767
+// Posted by Burak SARI
+// Retrieved 2026-04-25, License - CC BY-SA 4.0
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
 };
 
-createAuthRefresh(apiClient, refreshAuthLogic);
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err) => {
+    const originalRequest = err.config;
+
+    if (err.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = "Bearer " + token;
+            return axios(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      return new Promise(function (resolve, reject) {
+        apiClient
+          .post<LoginResponse>("/Auth/refresh-token")
+          .then(({ data }) => {
+            const decoded = jwtDecode<AppJwtPayload>(data.accessToken);
+            useAuthStore.getState().setAuth(data.accessToken, new Date(data.accessTokenExpiration).getTime(), decoded.Permission, decoded?.UserId, decoded?.email, decoded?.username);
+            originalRequest.headers["Authorization"] = "Bearer " + data.accessToken;
+            processQueue(null, data.accessToken);
+            resolve(apiClient(originalRequest));
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            reject(err);
+          })
+          .then(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+
+    return Promise.reject(err);
+  },
+);
