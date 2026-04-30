5a9a9fb6189f7138199a3ad6cb32207299abc2c7
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-04-30 03:09:08 +0300

    done

--- src/components/pos/layout/Sidebar.tsx
+++ src/components/pos/layout/Sidebar.tsx
@@ -235,7 +235,7 @@ export function OrdersDialog({ open, onOpenChange }: OrdersDialogProps) {
                             title="استكمال الدفع"
                             onClick={() => {
                               setScreen("home");
-                              console.log(order?.items)
+                              console.log(order?.items);
                               setCart(
                                 order.items.map((item) => ({
                                   productId: item.productId,
