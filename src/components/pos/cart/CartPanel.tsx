e69cc9c4c7b533f5fba765a0d4c32c057f250f67
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-05-01 05:26:17 +0300

    done

--- src/components/pos/cart/CartPanel.tsx
+++ src/components/pos/cart/CartPanel.tsx
@@ -803,7 +803,7 @@ export default function CartPanel() {
                 {/* Pay button */}
                 <Button
                   size={"2xl"}
-                  disabled={dineInMode !== "add-items"}
+                  disabled={dineInMode == "add-items"}
                   onClick={async () => {
                     if (cart.length === 0) {
                       notifyError(t("add_items_to_invoice"));
