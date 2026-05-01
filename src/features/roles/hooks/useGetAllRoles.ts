38238d4157b41091a6b529a4dec83a6e417358c2
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-05-01 09:04:57 +0300

    done

--- src/features/roles/hooks/useGetAllRoles.ts
+++ src/features/roles/hooks/useGetAllRoles.ts
@@ -2,10 +2,17 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { rolesKeys } from "../keys/roles.keys";
 import { getAllRoles } from "../services/roles";
 import { GetAllRolesResponse } from "../types/roles.types";
+import { handleEmptyResponse } from "@/lib/handleEmptyResponse";
 
 export const useGetAllRoles = ({ page, limit }: { page: number; limit: number }) =>
   useQuery<GetAllRolesResponse>({
     queryKey: rolesKeys.list({ page, limit }),
-    queryFn: () => getAllRoles({ page, limit }),
+    queryFn: async () => {
+      try {
+        return await getAllRoles({ page, limit });
+      } catch (error) {
+        return handleEmptyResponse(error);
+      }
+    },
     placeholderData: keepPreviousData,
   });
